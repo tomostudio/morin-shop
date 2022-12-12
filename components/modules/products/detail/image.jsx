8e67b8eed3512c3fa260881bdfbdcbc83adf9781
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-12-12 15:59:52 +0700

    💄 update min height product slider

--- components/modules/products/detail/image.jsx
+++ components/modules/products/detail/image.jsx
@@ -8,7 +8,7 @@ const PDImage = ({ sliderImage }) => {
   const [getIndex, setIndex] = useState(0)
 
   return (
-    <div className="w-full md:w-1/2 md:h-[calc(100vh-105px)] flex flex-col">
+    <div className="w-full md:w-1/2 md:h-[calc(100vh-105px)] md:min-h-[720px] flex flex-col">
       <div className="relative hidden md:block w-full h-full rounded-3xl overflow-hidden">
         {sliderImage[getIndex]?.image && (
           <Image
